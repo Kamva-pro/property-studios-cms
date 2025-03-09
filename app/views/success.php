719ad17f98a8a56ee176668e5067c475e85c6132
Kamvelihle Fatman <67002622+Kamva-pro@users.noreply.github.com>
2025-03-10 01:46:32 +0200

    Update success.php

--- app/views/success.php
+++ app/views/success.php
@@ -10,8 +10,8 @@ include __DIR__ . '/components/navbar.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<p>Thank you for submitting the form</p>
-
+<h1 style="text-align: center;">Thank you for submitting the form</h4>
+<h2 style="text-align: center;">We shall be in touch with you soon.</h6>
 <script>
      setTimeout(function() {
         window.location.href = "<?php echo BASE_PATH; ?>/"; 
