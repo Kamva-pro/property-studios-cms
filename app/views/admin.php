443b3dcd80946c5f15757a89f830269ef59ae27a
Kamva-pro <kamvelihlefatman@gmail.com>
2025-03-09 04:27:40 +0200

    converted datetime to readable format

--- app/views/admin.php
+++ app/views/admin.php
@@ -16,7 +16,7 @@ include __DIR__ . "/components/navbar.php";
     <table>
         <thead>
             <tr>
-                <th>ID</th>
+                <!-- <th>ID</th> -->
                 <th>Name</th>
                 <th>Email</th>
                 <th>Message</th>
@@ -26,11 +26,14 @@ include __DIR__ . "/components/navbar.php";
         <tbody>
             <?php foreach ($submissions as $submission): ?>
                 <tr>
-                    <td><?php echo htmlspecialchars($submission['id']); ?></td>
                     <td><?php echo htmlspecialchars($submission['name']); ?></td>
                     <td><?php echo htmlspecialchars($submission['email']); ?></td>
                     <td><?php echo htmlspecialchars($submission['message']); ?></td>
-                    <td><?php echo htmlspecialchars($submission['submitted_at']); ?></td>
+                    <td><?php
+                        $date = new DateTime($submission['submitted_at']);
+                        echo $date->format('F j, Y, g:i A');
+                        ?>
+                    </td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
